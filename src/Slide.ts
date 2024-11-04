cab5af3c2f73c220c51a99c59a0b56d6f741a525
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2024-11-04 11:41:35 -0800

    Prev & Next

--- src/Slide.ts
+++ src/Slide.ts
@@ -19,7 +19,7 @@ export default class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
 
   hide(el: Element) {
@@ -31,5 +31,32 @@ export default class Slide {
     this.slide = this.slides[this.index];
     this.slides.forEach((slide) => this.hide(slide));
     this.slide.classList.add("active");
+  }
+
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+
+  next() {
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement("button");
+    const nextButton = document.createElement("button");
+    prevButton.innerText = "Previous Slide";
+    nextButton.innerText = "Next Slide";
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    nextButton.addEventListener("pointerup", () => this.next());
+    prevButton.addEventListener("pointerup", () => this.prev());
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
